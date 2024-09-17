a249ebc84448a9563172f67585f4a85d3efcf18b
Mladen Kliska <mladen.kliska@roundfeather.com>
2024-09-17 14:33:30 -0400

    [PT-4915] tweaking headerFactory

--- src/main/java/com/roundfeather/paven/core/update/api/headers/TenantHeaderFactory.java
+++ src/main/java/com/roundfeather/paven/core/update/api/headers/TenantHeaderFactory.java
@@ -1,6 +1,5 @@
 package com.roundfeather.paven.core.update.api.headers;
 
-import com.roundfeather.paven.core.update.api.utils.GCPAuthUtil;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.core.MultivaluedMap;
 import org.eclipse.microprofile.rest.client.ext.ClientHeadersFactory;
@@ -10,9 +9,6 @@ public class TenantHeaderFactory implements ClientHeadersFactory {
 
     @Override
     public MultivaluedMap<String, String> update(MultivaluedMap<String, String> inboundHeaders, MultivaluedMap<String, String> outboundHeaders) {
-        outboundHeaders.add("Authorization", String.format("Bearer %s", GCPAuthUtil.getAuthToken()));
-        outboundHeaders.add("platform", "web");
-        outboundHeaders.add("os", "desktop");
-        return outboundHeaders;
+        return inboundHeaders;
     }
 }
